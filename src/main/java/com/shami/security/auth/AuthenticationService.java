4cdb268292f0c5eb8b907ba9ce44831b4ce3217d
shamision <shami0sion@gmail.com>
2023-05-29 16:41:10 +0300

    first commit

---
+++ src/main/java/com/shami/security/auth/AuthenticationService.java
@@ -0,0 +1,48 @@
+package com.shami.security.auth;
+
+import com.shami.security.config.JwtService;
+import com.shami.security.user.Role;
+import com.shami.security.user.User;
+import com.shami.security.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        Role role = Role.valueOf(request.getRole());
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(role)
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse
+                .builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+        var user  = repository.findByEmail(request.getEmail()).orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse
+                .builder()
+                .token(jwtToken)
+                .build();
+    }
+}
---
+++ src/main/java/com/shami/security/user/Role.java
@@ -0,0 +1,6 @@
+package com.shami.security.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
